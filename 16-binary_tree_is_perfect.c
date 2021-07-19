0b15a99e056c713111dce4c884909c064125d8ae
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-19 17:44:01 +0300

    perfect tree corrected betty

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -37,8 +37,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-        if (!tree->left && !tree->right)
-                return (1);
+	if (!tree->left && !tree->right)
+		return (1);
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left) &&
 			binary_tree_is_full(tree->right));
