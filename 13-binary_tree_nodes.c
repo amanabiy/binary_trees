e186914cc48614e622d8eb8110e6a6162626d319
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-19 16:09:28 +0300

    nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - conts the nodes with at least 1 child
+ * @tree: pointer to the tree
+ * Return: the number of the node, otherwise 0
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+	size_t nodes_L, nodes_R;
+
+	nodes_L = binary_tree_nodes(tree->left);
+	nodes_R = binary_tree_nodes(tree->right);
+
+	return (nodes_L + nodes_R + 1);
+}
