88d22ac095e3498240c367db21b848bdd31cc5f5
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-19 17:47:08 +0300

    depth added a blanck line after declaration

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,8 +8,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree || !tree->parent)
 		return (0);
-
 	size_t depth;
+
 	depth = binary_tree_depth(tree->parent);
 
 	return (depth + 1);
