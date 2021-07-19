2404b32f718cb1f9825570a332b3595ee907889e
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-19 13:09:16 +0300

    delete a binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -33,4 +34,6 @@ void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
+
 #endif
