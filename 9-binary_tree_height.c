3d76b24741a2e4f6e99aca2b10096092a31ee0f4
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-21 11:28:08 +0300

    declartion first

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,9 +8,8 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	
 	size_t left_height, right_height;
-	
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 
