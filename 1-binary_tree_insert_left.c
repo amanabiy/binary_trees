f96f985ed9d8fb3441aa707876337406354e57eb
Amanuel Abiy <amanuelabiy.as@gmail.com>
2021-07-21 10:59:42 +0300

    left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,26 +10,28 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new_left_node = NULL;
+
 	if (parent == NULL)
 		return (NULL);
-	binary_tree_t *new_left_child = malloc(sizeof(binary_tree_t));
 
-	if (new_left_child == NULL)
+	new_left_node = malloc(sizeof(binary_tree_t));
+	if (new_left_node == NULL)
 		return (NULL);
 
-	new_left_child->parent = parent;
-	new_left_child->n = value;
-	new_left_child->right = NULL;
+	new_left_node->parent = parent;
+	new_left_node->n = value;
 
-	if (parent->left == NULL)
+	if (parent->left)
 	{
-		new_left_child->left = NULL;
+		new_left_node->left = parent->left;
+		new_left_node->left->parent = new_left_node;
 	}
 	else
-	{
-		new_left_child->left = parent->left;
-		new_left_child->left->parent = new_left_child;
-	}
-	parent->left = new_left_child;
-	return (new_left_child);
+		new_left_node->left = NULL;
+
+	new_left_node->right = NULL;
+	parent->left = new_left_node;
+
+	return (new_left_node);
 }
