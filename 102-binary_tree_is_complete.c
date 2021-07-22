7eea822e5343fce30f25520c23c052fd6a0f1c2b
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-22 23:08:42 +0300

    left-rotation

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -7,9 +7,10 @@
  */
 binary_tree_t **createQueue(int *front, int *rear)
 {
-    binary_tree_t **queue = (binary_tree_t **)malloc(sizeof(binary_tree_t *) * 500);
-    *front = *rear = 0;
-    return queue;
+	binary_tree_t **queue = (binary_tree_t **)malloc(
+		sizeof(binary_tree_t *) * 500);
+	*front = *rear = 0;
+	return (queue);
 }
 /**
  * enQueueu - add to queue
@@ -19,8 +20,8 @@ binary_tree_t **createQueue(int *front, int *rear)
  */
 void enQueue(binary_tree_t **queue, int *rear, binary_tree_t *new_node)
 {
-    queue[*rear] = new_node;
-    (*rear)++;
+	queue[*rear] = new_node;
+	(*rear)++;
 }
 /**
  * deQueueu - add to queue
@@ -29,8 +30,8 @@ void enQueue(binary_tree_t **queue, int *rear, binary_tree_t *new_node)
  */
 binary_tree_t *deQueue(binary_tree_t **queue, int *front)
 {
-    (*front)++;
-    return queue[*front - 1];
+	(*front)++;
+	return (queue[*front - 1]);
 }
 
 
@@ -42,10 +43,10 @@ binary_tree_t *deQueue(binary_tree_t **queue, int *front)
  */
 int isQueueEmpty(int *front, int *rear)
 {
-   if (*rear == *front)
-        return (1);
-    else
-        return (0);
+	if (*rear == *front)
+		return (1);
+	else
+		return (0);
 }
 
 
@@ -54,36 +55,35 @@ int isQueueEmpty(int *front, int *rear)
  * @tree: a pointer to the root
  * Return: 1 if complete, otherwise 0
  */
- int binary_tree_is_complete(const binary_tree_t *tree)
- {
-    int rear, front;
-    binary_tree_t **queue = createQueue(&front, &rear);
-    int flag = 0;
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	 int rear, front;
+	 binary_tree_t **queue = createQueue(&front, &rear);
+	 int flag = 0;
 
-    if (tree == NULL)
-        return (0);
-    
-    enQueue(queue,&rear,(binary_tree_t *) tree);
-    while (!isQueueEmpty(&front, &rear))
-    {
-        binary_tree_t *temp_node = deQueue(queue, &front);
+	 if (tree == NULL)
+		 return (0);
+	 enQueue(queue, &rear, (binary_tree_t *)tree);
+	 while (!isQueueEmpty(&front, &rear))
+	 {
+		 binary_tree_t *temp_node = deQueue(queue, &front);
 
-        if (temp_node->left)
-        {
-            if (flag == 1)
-                return 0;
-            enQueue (queue, &rear, temp_node->left);
-        }
-        else
-            flag = 1;
-        if (temp_node->right)
-        {
-            if (flag == 1)
-                return (0);
-            enQueue (queue, &rear, temp_node->right);
-        }
-        else
-            flag = 1;
-    }
-    return (1);
- }
+		 if (temp_node->left)
+		 {
+			 if (flag == 1)
+				 return (0);
+			 enQueue (queue, &rear, temp_node->left);
+		 }
+		 else
+			 flag = 1;
+		 if (temp_node->right)
+		 {
+			 if (flag == 1)
+				 return (0);
+			 enQueue (queue, &rear, temp_node->right);
+		 }
+		 else
+			 flag = 1;
+	 }
+	 return (1);
+}
