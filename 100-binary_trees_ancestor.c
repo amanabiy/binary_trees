a1ce12ade9cf084d7e4308d1394a5238aa8a6faf
Amanuel Sisay <amanuelabiy.as@gmail.com>
2021-07-23 01:52:28 +0300

    using back trace from each node and finding there intersection

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,44 +1,5 @@
 #include "binary_trees.h"
 /**
- * find_the_root - finds the root of a binary tree
- * @node: a pointer to the node
- * Return: pointer to the root
- */
-binary_tree_t *find_the_root(const binary_tree_t *node)
-{
-	binary_tree_t *new_node = (binary_tree_t *)node;
-
-	if (node->parent == NULL)
-		return (new_node);
-	return (node->parent);
-}
-/**
- * bt_ancestor - finds the lca
- * @root: pointer to the root
- * @first: pointer to the first node
- * @second: pointer to the second node
- * Return: the lca, otherwise 0
- */
-binary_tree_t *bt_ancestor(binary_tree_t *root,
-			   const binary_tree_t *first,
-			   const binary_tree_t *second)
-{
-	binary_tree_t *left, *right;
-
-	if (root == NULL)
-		return (NULL);
-	if (root->n == first->n || root->n == second->n)
-		return (root);
-	left = bt_ancestor(root->left, first, second);
-	right = bt_ancestor(root->right, first, second);
-	if (left == NULL)
-		return (right);
-	if (right == NULL)
-		return (left);
-	return (root);
-}
-
-/**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: is a pointer to the first node
  * @second: is a pointer to the second node
@@ -47,10 +8,23 @@ binary_tree_t *bt_ancestor(binary_tree_t *root,
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 									const binary_tree_t *second)
 {
-	binary_tree_t *lca;
+	binary_tree_t *temp;
 
-	if (!first || !second)
+	if (first == NULL || second == NULL)
 		return (NULL);
-	lca = bt_ancestor(find_the_root(first), first, second);
-	return (lca);
+
+	temp = (binary_tree_t *)second;
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = temp;
+		first = first->parent;
+	}
+	return (NULL);
+
 }
